bb075833b6747990884326b159aee1ef36a263bc
29691 <2969163066@qq.com>
2024-07-13 16:43:31 +0800

    Commit Message

---
+++ main.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('Hello')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+单行注释
+多行注释
+"""
+# 单行注释
+# print(666)
+# print("hello")
+money = 50
+print("当前钱包余额:", money,"元")
+print("购买了冰淇淋，花费：10元")
+print("购买了可乐，花费：10元")
+print("最终，钱包剩余：",money-10-10,"元")
+
+print(type("黑马程序员"))
+print(type(666))
+print(type(11.345))
+name = "黑马程序员"
+name_type = type(name)
+print(name_type)
+num_str = str(11)
+float_str = str(11)
+print(type(num_str),num_str)
+print(type(float_str),float_str)
+num = int("11")
+print(type(num),num)
+
+num2 = float("11.345")
+print(type(num2),num2)
+
+num3 = int("黑马程序员")
+print(type(num3),num3)
